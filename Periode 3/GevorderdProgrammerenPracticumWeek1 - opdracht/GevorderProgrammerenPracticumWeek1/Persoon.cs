60eb3721b07563c2d9e9d5d1d9f15e56e65baa7f
dantheman0125 <danielroskam@gmail.com>
2018-02-06 16:04:33 +0100

    periode 1 af

---
+++ Periode 3/GevorderdProgrammerenPracticumWeek1 - opdracht/GevorderProgrammerenPracticumWeek1/Persoon.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GevorderProgrammerenPracticumWeek1
+{
+    class Persoon
+    {
+        private string voornaam;
+        private string achternaam;
+        private int leeftijd;
+
+        private List<Huisdier> huisdieren;
+
+        public Persoon(string voornaam, string achternaam, int leeftijd)
+        {
+            this.voornaam = voornaam;
+            this.achternaam = achternaam;
+            this.leeftijd = leeftijd;
+            huisdieren = new List<Huisdier>();
+        }
+
+        public string getNaam()
+        {
+            return voornaam+achternaam;
+        }
+
+        public List<Huisdier> getHuisdieren()
+        {
+
+        }
+
+        public void addHuisdier(Huisdier huisdier)
+        {
+
+        }
+
+        public int getLeeftijd()
+        {
+            return leeftijd;
+        }
+    }
+}
